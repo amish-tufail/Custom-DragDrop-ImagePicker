c430d6ed9c148f0013431cc07988332fc924551c
Amish <amish.tufail2002@gmail.com>
2024-02-02 19:28:28 +0500

    Initial Commit

---
+++ CustomDragDropImagePicker/CustomDragDropImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDragDropImagePickerApp.swift
+//  CustomDragDropImagePicker
+//
+//  Created by Amish Tufail on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomDragDropImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
